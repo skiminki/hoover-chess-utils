cec87982798af8426ace58e2ab6a3143e68d3504
Sami Kiminki <46296557+skiminki@users.noreply.github.com>
2025-08-22 13:02:00 +0300

    Add C++ header guardian check and update the guardians
    Fix also the expected copyright year starting range in
    scripts/check-source-preambles.py.

--- utils/memory-mapped-file.h
+++ utils/memory-mapped-file.h
@@ -14,8 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#ifndef CHESS_UTILS__UTILS__MEMORY_MAPPED_FILE_H_INCLUDED
-#define CHESS_UTILS__UTILS__MEMORY_MAPPED_FILE_H_INCLUDED
+#ifndef HOOVER_CHESS_UTILS__UTILS__MEMORY_MAPPED_FILE_H_INCLUDED
+#define HOOVER_CHESS_UTILS__UTILS__MEMORY_MAPPED_FILE_H_INCLUDED
 
 #include <cstddef>
 #include <string_view>
